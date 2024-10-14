043de538d7a8b9d4a408afed1f4652ddc15f5b7f
Nikita Skryabin <nekit2271@gmail.com>
2024-10-14 19:50:30 +0300

    feat(rkn-srs.py): add basic script logic

--- rkn-srs.py
+++ rkn-srs.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+import argparse
+
+
+def main(input_file, output_file): ...
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Script to convert domains list to srs sing box format',
+    )
+    parser.add_argument('-i', '--input', help='Input file path')
+    parser.add_argument('-o', '--output', help='Output file path')
+    args = parser.parse_args()
+    main(args.input, args.output)
