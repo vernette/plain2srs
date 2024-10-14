89cfd1ea5c23e38e4c4e492572362c14c5a5ad07
Nikita Skryabin <nekit2271@gmail.com>
2024-10-14 19:56:43 +0300

    feat(rkn-srs.py): add argument types, make the input file argument required

--- rkn-srs.py
+++ rkn-srs.py
@@ -8,9 +8,11 @@ def main(input_file, output_file): ...
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Script to convert domains list to srs sing box format',
+        description='Script to convert domain list to srs sing box format',
     )
-    parser.add_argument('-i', '--input', help='Input file path')
-    parser.add_argument('-o', '--output', help='Output file path')
+    parser.add_argument(
+        '-i', '--input', type=str, help='Input file path', required=True
+    )
+    parser.add_argument('-o', '--output', type=str, help='Output file path')
     args = parser.parse_args()
     main(args.input, args.output)
