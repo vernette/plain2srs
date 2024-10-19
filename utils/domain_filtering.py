befcb14d6234b42a1c7c131a988ada456e02d9cd
Nikita Skryabin <nekit2271@gmail.com>
2024-10-19 15:19:00 +0300

    feat(domain_filtering.py): add docstrings

--- utils/domain_filtering.py
+++ utils/domain_filtering.py
@@ -16,6 +16,23 @@ from core.constants import (
 
 
 async def get_antifilter_domains(timeout: int = REQUEST_TIMEOUT) -> list[str]:
+    """Fetch domains from the antifilter URL.
+
+    This function retrieves a list of domains from the specified antifilter
+    URL. It handles connection errors and timeouts appropriately.
+
+    Args:
+        timeout (int): The timeout for the request in seconds. Defaults to
+            REQUEST_TIMEOUT.
+
+    Returns:
+        list[str]: A list of domains retrieved from the antifilter URL.
+
+    Raises:
+        TimeoutError: If the request times out.
+        ClientConnectionError: If there is a connection error.
+        Exception: For any other unexpected errors.
+    """
     try:
         timeout_obj = ClientTimeout(total=timeout)
         async with ClientSession(timeout=timeout_obj) as session:
@@ -41,6 +58,17 @@ async def get_antifilter_domains(timeout: int = REQUEST_TIMEOUT) -> list[str]:
 
 
 def extract_second_level_domains(domains: list) -> set[str]:
+    """Extract second-level domains from a list of domains.
+
+    This function takes a list of domains and returns a set of
+    second-level domains by removing subdomains.
+
+    Args:
+        domains (list): A list of domain names.
+
+    Returns:
+        set[str]: A set of second-level domains.
+    """
     return {
         '.'.join(domain.split('.')[-SECOND_LEVEL_DOMAIN_PARTS:])
         for domain in domains
@@ -49,6 +77,17 @@ def extract_second_level_domains(domains: list) -> set[str]:
 
 
 def filter_domains_by_keywords(domains: set) -> set[str]:
+    """Filter domains based on specified keywords.
+
+    This function removes domains that contain any of the specified
+    keywords.
+
+    Args:
+        domains (set): A set of domain names to filter.
+
+    Returns:
+        set[str]: A set of domains that do not contain the specified keywords.
+    """
     for keyword in DOMAIN_KEYWORDS:
         domains = {domain for domain in domains if keyword not in domain}
     return domains
